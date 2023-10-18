0107ffd60be4ce173d0d8b7b55fd93a132102b41
Alejo Boga <alejoboga20@gmail.com>
2023-10-18 11:19:03 -0300

    config router

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,9 @@
 	<header>
 		<img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 	</header>
-	<main>
-		<AboutPage />
-	</main>
+	<router-view></router-view>
 </template>
 
-<script setup lang="ts">
-import AboutPage from '@/modules/pokemon/pages/AboutPage.vue';
-</script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
