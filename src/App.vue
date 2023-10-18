062b6e11461e5b20920563a067fdbedc4db3aedc
Alejo Boga <alejoboga20@gmail.com>
2023-10-18 12:00:36 -0300

    navigate with router

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
 	<header>
+		<SharedNavbar />
 		<img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 	</header>
-	<router-view></router-view>
+	<router-view> </router-view>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SharedNavbar from '@/modules/shared/components/Navbar.vue';
+</script>
 
-<style scoped></style>
+<style scoped>
+header {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+</style>
---
+++ src/modules/shared/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<RouterLink to="/">Pokemon List</RouterLink>
+		<RouterLink to="/id">Pokemon by id</RouterLink>
+		<RouterLink to="/about">About</RouterLink>
+	</div>
+</template>
+
+<script lang="ts">
+export default {
+	name: 'Navbar',
+};
+</script>
+
+<style lang="css" scoped>
+div {
+	padding: 0 32px;
+}
+
+div a {
+	color: #2c3e50;
+	font-weight: bold;
+	margin: 10px;
+}
+</style>
