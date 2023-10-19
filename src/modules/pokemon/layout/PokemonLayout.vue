0f7dce82e5166143d56204a78e144dccf5633ce3
Alejo Boga <alejoboga20@gmail.com>
2023-10-19 10:54:21 -0300

    child routes

---
+++ src/modules/pokemon/layout/PokemonLayout.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="pokemon-layout">
+		<h1>Pokemon Layout</h1>
+		<router-view></router-view>
+	</div>
+</template>
+<script>
+export default {
+	name: 'PokemonLayout',
+};
+</script>
+
+<style lang="css" scoped>
+.pokemon-layout {
+	background-color: deeppink;
+}
+</style>
--- src/router/router.ts
+++ src/router/router.ts
@@ -6,35 +6,42 @@ const routes: RouteRecordRaw[] = [
 		redirect: { name: 'home' },
 	},
 	{
-		path: '/home',
-		name: 'home',
-		component: () =>
-			import(/* webpackChunkName: "ListPage" */ '@/modules/pokemon/pages/ListPage.vue'),
-	},
-	{
-		path: '/about',
-		name: 'about',
-		component: () =>
-			import(/* webpackChunkName: "AboutPage" */ '@/modules/pokemon/pages/AboutPage.vue'),
-	},
-	{
-		path: '/pokemon/:pokemonId',
-		name: 'pokemon-id',
-		props: (route) => {
-			const id = Number(route.params.pokemonId);
-
-			if (isNaN(id)) return { id: 1 };
-
-			return { id };
-		},
-		component: () =>
-			import(/* webpackChunkName: "PokemonPage" */ '@/modules/pokemon/pages/PokemonPage.vue'),
-	},
-	{
 		path: '/:pathMatch(.*)*',
 		name: 'not-found',
 		component: () =>
 			import(/* webpackChunkName: "NotFoundPage" */ '@/modules/shared/pages/NotFoundPage.vue'),
+	},
+	{
+		path: '/pokemon',
+		name: 'pokemon',
+		component: () => import('@/modules/pokemon/layout/PokemonLayout.vue'),
+		children: [
+			{
+				path: '',
+				name: 'home',
+				component: () =>
+					import(/* webpackChunkName: "ListPage" */ '@/modules/pokemon/pages/ListPage.vue'),
+			},
+			{
+				path: 'about',
+				name: 'about',
+				component: () =>
+					import(/* webpackChunkName: "AboutPage" */ '@/modules/pokemon/pages/AboutPage.vue'),
+			},
+			{
+				path: 'pokemon-id/:pokemonId',
+				name: 'pokemon-id',
+				props: (route) => {
+					const id = Number(route.params.pokemonId);
+
+					if (isNaN(id)) return { id: 1 };
+
+					return { id };
+				},
+				component: () =>
+					import(/* webpackChunkName: "PokemonPage" */ '@/modules/pokemon/pages/PokemonPage.vue'),
+			},
+		],
 	},
 ];
 
